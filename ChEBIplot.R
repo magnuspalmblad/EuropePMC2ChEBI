99aa6105cd9a0309e42322c2f84419ba979a7f05
Magnus Palmblad <n.m.palmblad@lumc.nl>
2019-06-20 18:08:29 +0200

    Added TFIDF functionality

--- ChEBIplot.R
+++ ChEBIplot.R
@@ -33,12 +33,15 @@ WDW<-matrix(c(3.435E-09, 2.2213E-08, 1.08546E-07, 4.17925E-07, 1.25836E-06, 2.96
               8.9655E-08, 5.79769E-07, 2.8331E-06, 1.0908E-05, 3.28436E-05, 7.73962E-05, 0.000142749, 0.000206099, 0.000232936, 0.000206099, 0.000142749, 7.73962E-05, 3.28436E-05, 1.0908E-05, 2.8331E-06, 5.79769E-07, 8.9655E-08,
               3.435E-09, 2.2213E-08, 1.08546E-07, 4.17925E-07, 1.25836E-06, 2.96532E-06, 5.46921E-06, 7.89638E-06, 8.92459E-06, 7.89638E-06, 5.46921E-06, 2.96532E-06, 1.25836E-06, 4.17925E-07, 1.08546E-07, 2.2213E-08, 3.435E-09), nrow=17, ncol=9, byrow=FALSE)
 
+A<-read.table('ChEBI_counts_2015_to_2017.tsv')
+N<-972037 # the number of unique papers with text mined annotations in the 2015-2017 corpus
+
 scaling<-function(intensity, root)
 {
   return(intensity^(1/root))
 }
 
-plot1<-function(S1, root=4, normalize=TRUE, xmin=-5.5, xmax=10.5, ymin=0, ymax=1000, blur=TRUE)
+plot1<-function(S1, root=4, normalize=TRUE, xmin=-5.5, xmax=10.5, ymin=0, ymax=1000, blur=TRUE, tfidf=FALSE)
 {
   if(xmin< -5.5) xmin<- -5.5
   if(xmin>10.5) xmin<-10.5
@@ -57,13 +60,26 @@ plot1<-function(S1, root=4, normalize=TRUE, xmin=-5.5, xmax=10.5, ymin=0, ymax=1
   TEMP1<-matrix(0, nrow=280, ncol=268)
   S1x<-floor(16*S1$V2+96)
   S1y<-floor((S1$V1+6.25)/6.25); S1y[is.na(S1y)]<-FALSE
+  S1idf<-rep(0,length(S1x))
+  for(i in 1:length(S1x)) S1idf[i]<-log(N/(1+(A[which(A[,1]==S1$V3[i]),2])))
   
   max_s1<-0;
-  for(x in 9:264)
-  {
-    for(y in 5:260)
+  if(!tfidf) {
+    for(x in 9:264)
     {
-      M1[x,y]<-sum((S1x==x-8)*(S1y==y-4))
+      for(y in 5:260)
+      {
+        M1[x,y]<-sum((S1x==x-8)*(S1y==y-4))
+      }
+    }
+  }
+  
+  if(tfidf) {
+    for(i in 1:length(S1x))
+    {
+      x<-S1x[i]-8
+      y<-S1y[i]-4
+      if ( (x>=1) && (x<=280) && (y>=1) && (y<=268) ) M1[x,y]<-M1[x,y]+S1idf[i]
     }
   }
   
@@ -89,12 +105,16 @@ plot1<-function(S1, root=4, normalize=TRUE, xmin=-5.5, xmax=10.5, ymin=0, ymax=1
     for(y in 1:260) 
     { 
       M[x,y]<-rgb(M1[x+8,y+4]/max_s1, M1[x+8,y+4]/max_s1, M1[x+8,y+4]/max_s1)
+      # M[x,y]<-rgb(M1[x+8,y+4]/max_s1, 0, 0) # red
+      # M[x,y]<-rgb(0, M1[x+8,y+4]/max_s1, 0) # green
+      # M[x,y]<-rgb(0, 0, M1[x+8,y+4]/max_s1) # blue
+      
       rect(x/16-6-0.03125, y*6.25-6.25-3.125, x/16-5.9375-0.03125, y*6.25-3.125, angle=0, col=M[x,y], border=NA)
     }
   }
 }
 
-plot2<-function(S1, S2, root=4, normalize=TRUE, xmin=-5.5, xmax=10.5, ymin=0, ymax=1000, blur=TRUE)
+plot2<-function(S1, S2, root=4, normalize=TRUE, xmin=-5.5, xmax=10.5, ymin=0, ymax=1000, blur=TRUE, tfidf=FALSE)
 {
   if(xmin< -5.5) xmin<- -5.5
   if(xmin>10.5) xmin<-10.5
@@ -115,16 +135,37 @@ plot2<-function(S1, S2, root=4, normalize=TRUE, xmin=-5.5, xmax=10.5, ymin=0, ym
   TEMP2<-matrix(0, nrow=280, ncol=268)
   S1x<-floor(16*S1$V2+96)
   S1y<-floor((S1$V1+6.25)/6.25); S1y[is.na(S1y)]<-FALSE
+  S1idf<-rep(0,length(S1x))
+  for(i in 1:length(S1x)) S1idf[i]<-log(N/(1+(A[which(A[,1]==S1$V3[i]),2])))
   S2x<-floor(16*S2$V2+96)
   S2y<-floor((S2$V1+6.25)/6.25); S2y[is.na(S2y)]<-FALSE
+  S2idf<-rep(0,length(S2x))
+  for(i in 1:length(S2x)) S2idf[i]<-log(N/(1+(A[which(A[,1]==S2$V3[i]),2])))
   
   max_s1<-0; max_s2<-0
-  for(x in 9:264)
-  {
-    for(y in 5:260) 
+  if(!tfidf) {
+    for(x in 9:264)
     {
-      M1[x,y]<-sum((S1x==x-8)*(S1y==y-4))
-      M2[x,y]<-sum((S2x==x-8)*(S2y==y-4))
+      for(y in 5:260) 
+      {
+        M1[x,y]<-sum((S1x==x-8)*(S1y==y-4))
+        M2[x,y]<-sum((S2x==x-8)*(S2y==y-4))
+      }
+    }
+  }
+  
+  if(tfidf) {
+    for(i in 1:length(S1x))
+    {
+      x<-S1x[i]-8
+      y<-S1y[i]-4
+      if ( (x>=1) && (x<=280) && (y>=1) && (y<=268) ) M1[x,y]<-M1[x,y]+S1idf[i]
+    }
+    for(i in 1:length(S2x))
+    {
+      x<-S2x[i]-8
+      y<-S2y[i]-4
+      if ( (x>=1) && (x<=280) && (y>=1) && (y<=268) ) M2[x,y]<-M2[x,y]+S2idf[i]
     }
   }
   
@@ -155,7 +196,7 @@ plot2<-function(S1, S2, root=4, normalize=TRUE, xmin=-5.5, xmax=10.5, ymin=0, ym
   }
 }
 
-plot3<-function(S1, S2, S3, root=4, normalize=TRUE, xmin=-5.5, xmax=10.5, ymin=0, ymax=1000, blur=TRUE)
+plot3<-function(S1, S2, S3, root=4, normalize=TRUE, xmin=-5.5, xmax=10.5, ymin=0, ymax=1000, blur=TRUE, tfidf=FALSE)
 {
   if(xmin< -5.5) xmin<- -5.5
   if(xmin>10.5) xmin<-10.5
@@ -178,19 +219,48 @@ plot3<-function(S1, S2, S3, root=4, normalize=TRUE, xmin=-5.5, xmax=10.5, ymin=0
   TEMP3<-matrix(0, nrow=280, ncol=268)
   S1x<-floor(16*S1$V2+96)
   S1y<-floor((S1$V1+6.25)/6.25); S1y[is.na(S1y)]<-FALSE
+  S1idf<-rep(0,length(S1x))
+  for(i in 1:length(S1x)) S1idf[i]<-log(N/(1+(A[which(A[,1]==S1$V3[i]),2])))
   S2x<-floor(16*S2$V2+96)
   S2y<-floor((S2$V1+6.25)/6.25); S2y[is.na(S2y)]<-FALSE
+  S2idf<-rep(0,length(S2x))
+  for(i in 1:length(S2x)) S2idf[i]<-log(N/(1+(A[which(A[,1]==S2$V3[i]),2])))
   S3x<-floor(16*S3$V2+96)
   S3y<-floor((S3$V1+6.25)/6.25); S3y[is.na(S3y)]<-FALSE
+  S3idf<-rep(0,length(S3x))
+  for(i in 1:length(S3x)) S3idf[i]<-log(N/(1+(A[which(A[,1]==S3$V3[i]),2])))
   
   max_s1<-0; max_s2<-0; max_s3<-0
-  for(x in 9:264)
-  {
-    for(y in 5:260)
+  if(!tfidf) {
+    for(x in 9:264)
     {
-      M1[x,y]<-sum((S1x==x-8)*(S1y==y-4))
-      M2[x,y]<-sum((S2x==x-8)*(S2y==y-4))
-      M3[x,y]<-sum((S3x==x-8)*(S3y==y-4))
+      for(y in 5:260)
+      {
+        M1[x,y]<-sum((S1x==x-8)*(S1y==y-4))
+        M2[x,y]<-sum((S2x==x-8)*(S2y==y-4))
+        M3[x,y]<-sum((S3x==x-8)*(S3y==y-4))
+      }
+    }
+  }
+  
+  if(tfidf) {
+    for(i in 1:length(S1x))
+    {
+      x<-S1x[i]-8
+      y<-S1y[i]-4
+      if ( (x>=1) && (x<=280) && (y>=1) && (y<=268) ) M1[x,y]<-M1[x,y]+S1idf[i]
+    }
+    for(i in 1:length(S2x))
+    {
+      x<-S2x[i]-8
+      y<-S2y[i]-4
+      if ( (x>=1) && (x<=280) && (y>=1) && (y<=268) ) M2[x,y]<-M2[x,y]+S2idf[i]
+    }
+    for(i in 1:length(S3x))
+    {
+      x<-S3x[i]-8
+      y<-S3y[i]-4
+      if ( (x>=1) && (x<=280) && (y>=1) && (y<=268) ) M3[x,y]<-M3[x,y]+S3idf[i]
     }
   }
   
@@ -206,14 +276,14 @@ plot3<-function(S1, S2, S3, root=4, normalize=TRUE, xmin=-5.5, xmax=10.5, ymin=0
   
   if(!blur) {TEMP1<-M1; TEMP2<-M2; TEMP3<-M3}
   if(normalize==TRUE) {TEMP1<-TEMP1/length(S1x); TEMP2<-TEMP2/length(S2x); TEMP3<-TEMP3/length(S3x)}
-  M1<-scaling(TEMP1[1:272,1:264], root); M2<-scaling(TEMP2[1:272,1:264], root);  M3<-scaling(TEMP3[1:272,1:264], root)
+  M1<-scaling(TEMP1[1:272,1:264], root); M2<-scaling(TEMP2[1:272,1:264], root); M3<-scaling(TEMP3[1:272,1:264], root)
   max_s1<-max(M1); max_s2<-max(M2); max_s3<-max(M3)
   
   plot(0, 0, type="n", xaxs="i", yaxs="i", col="black", xlim=c(xmin,xmax), ylim=c(ymin,ymax), xlab=expression('log'['10']*'P'['ow']), ylab='mass (Da)', xaxp=c(-5,10,15), yaxp=c(0,1600,16), las=1)
   
   for(x in 1:264)
   {
-    for(y in 1:260) 
+    for(y in 1:260)
     {    
       M[x,y]<-rgb(M1[x+8,y+4]/max_s1, M2[x+8,y+4]/max_s2, M3[x+8,y+4]/max_s3)
       rect(x/16-6-0.03125, y*6.25-6.25-3.125, x/16-5.9375-0.03125, y*6.25-3.125, angle=0, col=M[x,y], border=NA)
